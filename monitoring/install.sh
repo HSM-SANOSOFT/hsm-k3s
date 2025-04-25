b95657640564cace00ac8e7c08a3111be5566e5c
Raul <raulsantamariao@hotmail.com>
2025-04-25 15:39:49 +0000

    refactor: update install scripts for monitoring and security namespaces to use absolute paths for resource files

--- monitoring/install.sh
+++ monitoring/install.sh
@@ -5,25 +5,25 @@ helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
 # Create the namespace if not exists
-kubectl apply -f monitoring/namespace.yml
+kubectl apply -f /namespace.yml
 
 echo ">>> Installing Promtail prerequisites..."
-kubectl apply -f monitoring/promtail/cluster-role.yml -n $NAMESPACE
-kubectl apply -f monitoring/promtail/cluster-role-binding.yml -n $NAMESPACE
-kubectl apply -f monitoring/promtail/service-account.yml -n $NAMESPACE
-kubectl apply -f monitoring/promtail/configmap.yml -n $NAMESPACE
+kubectl apply -f /promtail/cluster-role.yml -n $NAMESPACE
+kubectl apply -f /promtail/cluster-role-binding.yml -n $NAMESPACE
+kubectl apply -f /promtail/service-account.yml -n $NAMESPACE
+kubectl apply -f /promtail/configmap.yml -n $NAMESPACE
 
 echo ">>> Installing Promtail..."
-helm upgrade --install promtail grafana/promtail -n $NAMESPACE -f monitoring/promtail/values.yml
+helm upgrade --install promtail grafana/promtail -n $NAMESPACE -f /promtail/values.yml
 
 echo ">>> Installing Loki..."
-helm upgrade --install loki grafana/loki -n $NAMESPACE -f monitoring/loki/values.yml
+helm upgrade --install loki grafana/loki -n $NAMESPACE -f /loki/values.yml
 
 echo ">>> Installing Mimir..."
-helm upgrade --install mimir grafana/mimir-distributed -n $NAMESPACE -f monitoring/mimir/values.yml
+helm upgrade --install mimir grafana/mimir-distributed -n $NAMESPACE -f /mimir/values.yml
 
 echo ">>> Installing Grafana ConfigMap..."
-kubectl apply -f monitoring/grafana/configmap.yml -n $NAMESPACE
+kubectl apply -f /grafana/configmap.yml -n $NAMESPACE
 
 echo ">>> Installing Grafana..."
-helm upgrade --install grafana grafana/grafana -n $NAMESPACE -f monitoring/grafana/values.yml
+helm upgrade --install grafana grafana/grafana -n $NAMESPACE -f /grafana/values.yml
--- security/infisical/install.sh
+++ security/infisical/install.sh
@@ -6,17 +6,17 @@ helm repo add infisical-helm-charts 'https://dl.cloudsmith.io/public/infisical/h
 helm repo update
 
 # Create the namespace if not exists
-kubectl apply -f security/namespace.yml
+kubectl apply -f /namespace.yml
 
 echo ">>> Installing Infisical prerequisites..."
 helm install --generate-name infisical-helm-charts/secrets-operator -n security
 
 echo ">>> Installing InfisicalSecert CRD..."
-kubectl apply -f security/infisical/cluster-role-binding.yml -n $NAMESPACE
-kubectl apply -f security/infisical/service-account.yml -n $NAMESPACE
-kubectl apply -f security/infisical/service-account-reviewer.yml -n $NAMESPACE
-kubectl apply -f security/infisical/secret-token-reviewer.yml -n $NAMESPACE
-kubectl apply -f security/infisical/infisical-config.yml -n $NAMESPACE
+kubectl apply -f /infisical/cluster-role-binding.yml -n $NAMESPACE
+kubectl apply -f /infisical/service-account.yml -n $NAMESPACE
+kubectl apply -f /infisical/service-account-reviewer.yml -n $NAMESPACE
+kubectl apply -f /infisical/secret-token-reviewer.yml -n $NAMESPACE
+kubectl apply -f /infisical/infisical-config.yml -n $NAMESPACE
 
 echo ">>> Seting up Infisical..."
 kubectl patch serviceaccount infisical-token-reviewer -p '{"secrets": [{"name": "infisical-token-reviewer-token"}]}' -n $NAMESPACE
