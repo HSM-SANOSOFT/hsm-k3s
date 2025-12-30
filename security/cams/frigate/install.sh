087ce90f8343ac2489465509897a0706d87d51f0
Raul <raulsantamariao@hotmail.com>
2025-12-30 21:34:07 +0000

    fix(frigate): disable admin password reset and update resource limits in deployment

--- security/cams/frigate/install.sh
+++ security/cams/frigate/install.sh
@@ -1,5 +1,5 @@
 kubectl  -n security delete configmap hsm-security-cams-frigate-configmap
-kubectl -n security create configmap hsm-security-cams-frigate-configmap --from-file=config.yml=/home/sanosoft/hsm-k3s/security/cams/frigate/config.yml
+kubectl -n security create configmap hsm-security-cams-frigate-configmap --from-file=config.yml=security/cams/frigate/config.yml
 kubectl -n security rollout restart deploy/hsm-security-cams-frigate
 POD=$(kubectl -n security get pod -l app=hsm-security-cams-frigate -o jsonpath='{.items[0].metadata.name}')
 echo "To view logs:"
